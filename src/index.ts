e919e2e1593011d6fe93f12cb36cd405107d4d05
tukuyomil032 <yinkom0708@gmail.com>
2026-01-28 23:40:41 +0900

    Fix: Issue where the create command does not enter interactive question (choice) mode

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { helloCommand } from './commands/hello.js';
 import { createCommand } from './commands/create.js';
 
 const program = new Command();
 program
   .name('mccmd')
   .version('0.0.0')
-  .description('Genarate Minecraft Java Edition command on CLI.');
-program.addCommand(helloCommand());
+  .description('Generate Minecraft Java Edition command on CLI.');
 program.addCommand(createCommand());
 program.parse(process.argv);
 
