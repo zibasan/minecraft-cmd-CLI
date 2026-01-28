fe9966675275c0ce6f0726544c3844ab1f2e2f81
zibasan <ZIBA3496@gmail.com>
2026-01-28 13:04:38 +0900

    Fix: tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import { helloCommand } from './commands/hello.js';
 
 const program = new Command();
 program
-  .name('Minecraft_cmd_CLI')
+  .name('mccmd')
   .version('0.0.0')
   .description('Genarate Minecraft Java Edition command on CLI.');
 program.addCommand(helloCommand());
 program.parse(process.argv);
+
+if (process.argv.length === 2) {
+  program.help();
+}
